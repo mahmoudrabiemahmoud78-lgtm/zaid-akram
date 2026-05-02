81190d35f7d61a78e45da1681e1a21cfcedba1c0
mahmoud <mahmoudrabiemahmoud78-lgtm>
2026-05-02 15:05:24 +0300

    mahmoud

---
+++ script.js
@@ -0,0 +1,38 @@
+let all = document.querySelectorAll('.animtion')
+console.log(all);
+let observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+
+},{
+    threshold:0.3
+});
+all.forEach(ele=>{
+    observer.observe(ele)
+})
+
+let btn = document.getElementById('float')
+addEventListener('scroll',function(){
+    let sc = window.scrollY
+    console.log(sc);
+    if(sc >= 480){
+        btn.style.display='block'
+    }
+    else{
+        btn.style.display='none'
+        
+    }
+    
+})
+btn.onclick=_=>{
+    scroll({
+        
+        top:0,
+        behavior:"smooth"
+
+    })
+}
